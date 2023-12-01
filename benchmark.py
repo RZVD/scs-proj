1a2b1b53a807b9f9e10d2fb932ba8f59fd908a78
RZVD <razvanfuca@gmail.com>
2023-12-01 10:11:41 +0200

    Saving data.csv with charts

--- benchmark.py
+++ benchmark.py
@@ -1,64 +1,109 @@
 #!/bin/python3
-
 import tkinter as tk
-from tkinter import filedialog
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from tkinter import filedialog 
 import random
-from matplotlib import subprocess
+from matplotlib import subprocess, shutil
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import json
 import time
 import yaml
+import argparse
 
+class AppGUI:
+    __default_config = {
+        'memory': {
+            'array_size': 100000,
+            'min': 0,
+            'max': 123546243,
+            'testcase_dest': './mem_testcase',
+            'show_outputs': True
+        },
+        'threading': {
+            'thread_launches': 100000,
+            'pipe_reads': 100000,
+            'show_outputs': False
+        },
+        'benchmark': {
+            'clean': True,
+        },
+        'compilation': {
+            'compile': False,
+            'show_outputs': False
+        },
+        'charts': {
+            'save': True,
+            'show': True,
+            'figsize': [15, 6]
+        }
+    }
 
-class App:
-    def __init__(self, root, config):
+    def __init__(self, root: tk.Tk, config: dict | None):
         self.root = root
-        self.root.title("Test Configuration")
+        self.root.title("Benchmarking Configuration")
+        self.__config = config if config is not None else self.__default_config
+        self.__create_widgets()
 
-        self.config = config
+    def __create_widgets(self):
+        self.entry_widgets = {}
 
-        self.create_widgets()
-
-    def create_widgets(self):
-        tk.Button(self.root, text="Select Config File", command=self.load_config).pack(pady=10)
+        tk.Button(self.root, text="Select Config File", command=self.__load_config).pack(pady=10)
         tk.Button(self.root, text="Run Tests", command=self.run_tests).pack(pady=10)
 
-        for section, options in self.config.items():
-            tk.Label(self.root, text=f"{section.capitalize()} Configuration").pack(pady=5, anchor="w")
-            for option, default_value in options.items():
+        for key, value in self.__config.items():
+            tk.Label(self.root, text=f"{key.capitalize()} Configuration").pack(pady=5, anchor="w")
+            for option, default_value in value.items():
                 frame = tk.Frame(self.root)
                 frame.pack(pady=2, padx=10, anchor="w")
                 tk.Label(frame, text=f"{option.capitalize()}:", width=15, anchor="w").pack(side="left")
-                if isinstance(default_value, bool):
-                    var = tk.BooleanVar()
-                    var.set(default_value)
-                    tk.Checkbutton(frame, variable=var).pack(side="left")
-                    self.config[section][option] = var
-                elif isinstance(default_value, list):
-                    var = tk.StringVar()
-                    var.set(str(default_value))
-                    tk.Entry(frame, textvariable=var).pack(side="left")
-                    self.config[section][option] = var
-                else:
-                    var = tk.IntVar() if isinstance(default_value, int) else tk.StringVar()
-                    var.set(default_value)
-                    tk.Entry(frame, textvariable=var).pack(side="left")
-                    self.config[section][option] = var
 
-    def load_config(self):
-        file_path = filedialog.askopenfilename(defaultextension=".json", filetypes=[("JSON Files", "*.json")])
+                if isinstance(default_value, bool):
+                    entry_var = tk.BooleanVar(value=default_value)
+                    entry_widget = tk.Checkbutton(frame, variable=entry_var)
+                elif option == 'figsize':
+                    entry_var = tk.StringVar(value=', '.join(map(str, default_value)))
+                    entry_widget = tk.Entry(frame, textvariable=entry_var)
+                else:
+                    entry_var = tk.StringVar(value=str(default_value))
+                    entry_widget = tk.Entry(frame, textvariable=entry_var)
+
+                entry_widget.pack(side="left")
+
+                self.entry_widgets[(key, option)] = entry_var
+
+    def __load_config(self):
+        file_path = filedialog.askopenfilename(filetypes=[("All files", "*.*"), ("JSON Files", "*.json"), ("YAML Files", "*.yaml")])
         if file_path:
             with open(file_path, 'r') as config_file:
-                self.config = json.load(config_file)
+                self.__config = yaml.safe_load(config_file)
             print(f"Config loaded from {file_path}")
+            self.__update_gui_from_config()
+
+    def __update_gui_from_config(self):
+        for (key, option), entry_var in self.entry_widgets.items():
+            config_value = self.__config.get(key, {}).get(option)
+            if isinstance(config_value, bool):
+                entry_var.set(config_value)
+            else:
+                entry_var.set(str(config_value))
+
+    def __update_config_from_gui(self):
+        for (key, option), entry_var in self.entry_widgets.items():
+            widget_value = entry_var.get()
+            try:
+                if option == 'figsize':
+                    widget_value = [int(x.strip()) for x in widget_value.split(',')]
+                else:
+                    widget_value = int(widget_value)
+            except ValueError:
+                pass
+
+            self.__config[key][option] = widget_value
 
     def run_tests(self):
-        # Execute the tests with the loaded or default config
-        main(self.config)
-
+        self.__update_config_from_gui()
+        benchmark(self.__config)
 
 
 def clean_data():
@@ -66,7 +111,7 @@ def clean_data():
         datafile.write("TestName,Language,Size,Duration\n")
 
 def build_apps(config):
-    if config['compile'] == True:
+    if config['compile']:
         capture_output = not config['show_outputs']
         for dir in ['memory', 'threading']:
             subprocess.run([
@@ -157,33 +202,88 @@ def parse_data(config):
     if config['save']:
         os.makedirs(f'charts/{timestamp}/')
 
+    print('parse_data')
     for test_name, group in grouped:
         plt.figure(figsize=config['figsize'])
         plt.bar(group['Language'], group['Duration'])
         plt.title(f'Duration for {test_name}')
         plt.xlabel('Language')
         plt.ylabel('Duration (ns)')
-        
         if config['save']:
             plt.savefig(f'charts/{timestamp}/{test_name}_chart.png')
+            shutil.copyfile('data.csv', f'charts/{timestamp}/data.csv')
         
         if config['show']:
             plt.show()
 
-def main(config):
+def benchmark(config):
     global timestamp
     timestamp = int(time.time())
     build_apps(config['compilation'])
     
     if config['benchmark']['clean']:
         clean_data()
+
     memory_tests(config['memory'])
     threading_tests(config['threading'])
+
     parse_data(config['charts'])
 
+def main(args):
+    if args.json:
+        print(args.json)
+        config = json.loads(args.json)
+    else:
+        with open('config.yaml', 'r') as config_file:
+            config = yaml.safe_load(config_file)
+
+        override_options = {
+            'memory': ['array_size', 'min', 'max', 'testcase_dest', 'show_outputs'],
+            'threading': ['thread_launches', 'pipe_reads', 'show_outputs'],
+            'benchmark': ['clean', 'show_outputs'],
+            'compilation': ['compile' ,'show_outputs'],
+            'charts': ['save', 'show', 'figsize']
+        }
+
+        for section, options in override_options.items():
+            for option in options:
+                cli_option = f'{section}_{option.replace("-", "_")}'
+                if getattr(args, cli_option) is not None:
+                    config[section][option] = getattr(args, cli_option)
+
+    if args.gui:
+        gui = AppGUI(
+            tk.Tk(), 
+            config
+        )
+        gui.root.mainloop()
+    else:
+        benchmark(config)
+
 if __name__ == '__main__':
-    root = tk.Tk()
-    with open('config.yaml', 'r') as config_file:
-        config = yaml.safe_load(config_file)
-    app = App(root, config)
-    root.mainloop()
+    parser = argparse.ArgumentParser(description="Benchmarking Tool")
+
+    parser.add_argument('--json', type=str, help='JSON string representing the config')
+    parser.add_argument('--gui', action='store_true', help='Launch with GUI')
+
+    parser.add_argument('--memory-array-size', type=int, help='Array size for memory tests')
+    parser.add_argument('--memory-min', type=int, help='Lower bound for elements generated for memory test')
+    parser.add_argument('--memory-max', type=int, help='Upper bound for elements generated for memory test')
+    parser.add_argument('--memory-testcase-dest', type=str, help='Destination for memory testcases')
+    parser.add_argument('--memory-show-outputs', type=bool, help='Show outputs for memory tests')
+
+    parser.add_argument('--threading-thread-launches', type=int, help='Number of thread launches for threading tests')
+    parser.add_argument('--threading-pipe-reads', type=int, help='Number of pipe reads for threading tests')
+    parser.add_argument('--threading-show-outputs', type=bool, help='Show outputs for threading tests')
+
+    parser.add_argument('--benchmark-clean', type=bool, help='Cleans data.csv')
+    parser.add_argument('--benchmark-show-outputs', type=bool, help='Don\'t remember what this was for. Not used in code yeat' )
+
+    parser.add_argument('--compile', action='store_true', dest='compilation_compile', help='Recompiles the apps before benchmarking')
+    parser.add_argument('--compilation-show-outputs', action='store_true', help='Show outputs for compilation')
+
+    parser.add_argument('--charts-save', type=bool, help='Save charts as images under charts/{timestamp}')
+    parser.add_argument('--charts-show', type=bool, help='Shows charts')
+    parser.add_argument('--charts-figsize', type=int, nargs='+', help='')
+    args = parser.parse_args()
+    main(args)
